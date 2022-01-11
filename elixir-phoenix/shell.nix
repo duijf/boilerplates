5d722ca673ab91e08eb72ce5f97bcecdc0306283
Laurens Duijvesteijn <git@duijf.io>
2022-01-11 20:01:47 +0100

    Update Elixir Nix config for macOS

--- elixir-phoenix/shell.nix
+++ elixir-phoenix/shell.nix
@@ -1,17 +1,29 @@
 let
   pkgs = import ./nix/nixpkgs.nix {};
-in
-pkgs.mkShell {
-  name = "phoenix-devenv";
-  buildInputs = [
-    pkgs.elixir_1_12    # Language runtime
-    pkgs.inotify-tools  # Filesystem watcher (phoenix dep)
+
+  common = [
+    pkgs.beam.packages.erlangR24.elixir_1_13 # Language runtime
+    pkgs.beam.packages.erlangR24.rebar3      # Erlang package manager
     pkgs.overmind       # Process runner
-    pkgs.postgresql_13  # Database
-    pkgs.rebar3         # Erlang package manager
+    pkgs.postgresql_14  # Database
+    pkgs.esbuild        # Assets
   ];
 
-  shellHook = ''
-    export MIX_REBAR3="$(which rebar3)"
-  '';
-}
+  linuxExtra = [
+    pkgs.inotify-tools  # File system watcher
+  ];
+
+  darwinExtra = [
+    pkgs.terminal-notifier  # For ExUnit terminal notifications
+    pkgs.darwin.apple_sdk.frameworks.CoreFoundation  # Support for C stuff.
+    pkgs.darwin.apple_sdk.frameworks.CoreServices    # Support for C stuff.
+  ];
+
+in
+  pkgs.mkShell {
+    name = "phoenix-devenv";
+
+    buildInputs = common ++
+      pkgs.lib.optionals pkgs.stdenv.isLinux linuxExtra ++
+      pkgs.lib.optionals pkgs.stdenv.isDarwin darwinExtra;
+  }
